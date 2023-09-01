c3eed06677e4510a14ffafe2444a375f77032210
AngieMLi <angelamli@outlook.com>
2023-08-31 23:53:21 -0700

    added prob8

---
+++ problem8.py
@@ -0,0 +1,9 @@
+# 8. Write a Python program to check if a list is empty or not.
+
+def listChecker(x):
+    if len(x) == 0:
+        print("This list is empty")
+    else:
+        print("This list is not empty")
+
+listChecker(['Geeks', 'Geeks', 'Geeks', 'Geeks', ])
